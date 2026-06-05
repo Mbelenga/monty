d102f03ee4990371c8b9725189bb3e3bed7e77dc
Mbelenga <mbelengaterence@gmail.com>
2026-06-06 00:37:09 +0300

    Header file

---
+++ monty.h
@@ -0,0 +1,30 @@
+#ifndef MONTY_H
+#define MONTY_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+
+typedef struct stack_s
+{
+	int n;
+	struct stack_s *prev;
+	struct stack_s *next;
+} stack_t;
+
+typedef struct instruction_s
+{
+	char *opcode;
+	void (*f)(stack_t **stack, unsigned int line_number);
+} instruction_t;
+
+extern char *arg;
+
+void push(stack_t **stack, unsigned int line_number);
+void pall(stack_t **stack, unsigned int line_number);
+
+int is_number(char *str);
+void free_stack(stack_t *stack);
+
+#endif
